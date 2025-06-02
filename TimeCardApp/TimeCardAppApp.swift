c9ea05acc21e54eb968e85f246b73f2290a1ee4d
dljardim <dljardim@outlook.com>
2025-06-02 10:37:30 -0500

    Initial Commit

---
+++ TimeCardApp/TimeCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCardAppApp.swift
+//  TimeCardApp
+//
+//  Created by Damian Jardim on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
